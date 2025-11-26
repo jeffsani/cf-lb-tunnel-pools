609b88d8dfe850983f195532238146f62a4a89cf
Jeff Sani <jeff.sani@gmail.com>
2025-11-25 20:07:33 -0800

    updated instructions in outputs.tf to harvest tunnel ids and tokens

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+//Use terraform output -raw primary_tunnel_token to get the token value without quotes from the cli
+//Use terraform output -raw primary_tunnel_id to get the tunnel ID value without quotes from the cli
+//Use terraform output -raw failover_tunnel_token to get the token value without quotes from the cli
+//Use terraform output -raw failover_tunnel_id to get the tunnel ID value without quotes from the cli
+
 output "primary_tunnel_id" {
   value = cloudflare_zero_trust_tunnel_cloudflared.primary.id
 }
